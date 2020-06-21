0db3f5e503e512ed2be83f956d8f0fea37f639b9
rykos <rykos.net@gmail.com>
2020-06-21 23:53:20 +0200

    make working design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -20,33 +21,75 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            margin: EdgeInsets.all(50),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  FlatButton(
-                    child: Text('Play'),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => QuizPage()));
-                    },
-                    color: Colors.blue,
-                  ),
-                  FlatButton(
-                    child: Text('Leaderboard'),
-                    onPressed: () {},
-                    color: Colors.blue,
-                  ),
-                ],
+      body: Container(
+        margin: EdgeInsets.all(50),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Play'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => QuizPage(),
+                    ),
+                  );
+                },
+                color: Colors.blue,
               ),
-            )));
+              FlatButton(
+                child: Text('Leaderboard'),
+                onPressed: () {},
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Quiz {
+  String question;
+  String correctAnswer;
+  List anwers = new List();
+  List buttons = new List();
+
+  Quiz() {
+    load();
+  }
+
+  void load() async {
+    Map<String, dynamic> json =
+        jsonDecode(await rootBundle.loadString('assets/quizes/quiz1.json'));
+    this.question = json['question'];
+    for (String s in json['incorrectAnswers']) {
+      this.anwers.add(s);
+    }
+    this.anwers.add(json['correctAnswers'][0]);
+    callAllButtons();
+  }
+
+  void registerButtonState(_QuizAnswerButtonState buttonState){
+    buttons.add(buttonState);
+  }
+
+  void callAllButtons(){
+    int counter = 0;
+    for(_QuizAnswerButtonState button in buttons){
+      button.updateButtonText(anwers[counter]);
+      counter++;
+    }
   }
 }
 
 class QuizPage extends StatelessWidget {
+  final Quiz quiz = new Quiz();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,15 +101,15 @@ class QuizPage extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                QuizAnswerButton(),
-                QuizAnswerButton(),
+                QuizAnswerButton(quiz: quiz),
+                QuizAnswerButton(quiz: quiz),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                QuizAnswerButton(),
-                QuizAnswerButton(),
+                QuizAnswerButton(quiz: quiz),
+                QuizAnswerButton(quiz: quiz),
               ],
             ),
           ],
@@ -86,20 +129,60 @@ class QuizQuestion extends StatelessWidget {
   }
 }
 
-class QuizAnswerButton extends StatelessWidget {
+class QuizAnswerButton extends StatefulWidget {
+  final Quiz quiz;
+  QuizAnswerButton({this.quiz});
+
+  @override
+  _QuizAnswerButtonState createState() => _QuizAnswerButtonState(quiz: quiz);
+}
+
+class _QuizAnswerButtonState extends State<QuizAnswerButton> {
+  String questionText = 'null';
+  Quiz quiz;
+  Color color = Colors.blue;
+
+  _QuizAnswerButtonState({this.quiz}){
+    this.quiz.registerButtonState(this);
+  }
+
+  void updateButtonText(String newText){
+    setState(() {
+      questionText = newText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      constraints: BoxConstraints(
+        maxHeight: 150,
+        maxWidth: 150,
+      ),
       margin: EdgeInsets.all(5),
       child: ButtonTheme(
         height: 150,
         minWidth: 150,
         child: FlatButton(
-          child: Text('data'),
-          onPressed: () {},
-          color: Colors.blueAccent,
+          child: Container(
+            child: Text(
+              questionText,
+              overflow: TextOverflow.clip,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          onPressed: () {
+            setState(() {
+              color = getUpdatedColor(true);
+            });
+          },
+          color: color,
         ),
       ),
     );
+  }
+
+  Color getUpdatedColor(bool answer) {
+    return (answer) ? Colors.green : Colors.red;
   }
 }
